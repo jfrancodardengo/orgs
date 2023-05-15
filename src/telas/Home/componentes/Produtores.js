bbd47b91ec01261b029c1d3eaffeebd3510a9348
030093643 <jadardengo@banestes.com.br>
2023-05-15 08:30:06 -0300

    feat: criação do topo e produtores como funções e hooks

---
+++ src/telas/Home/componentes/Produtores.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text} from 'react-native';
+
+import {carregaProdutores} from '../../../servicos/carregaDados';
+
+function Produtores({topo: Topo}) {
+  const [titulo, setTitutlo] = useState('');
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    const retorno = carregaProdutores();
+    setTitutlo(retorno.titulo);
+    setLista(retorno.lista);
+  }, []);
+
+  const TopoLista = () => {
+    return (
+      <>
+        <Topo />
+        <Text style={estilos.titulo}>{titulo}</Text>
+      </>
+    );
+  };
+
+  return (
+    <FlatList
+      data={lista}
+      renderItem={({item: {nome}}) => <Text>{nome}</Text>}
+      keyExtractor={({nome}) => nome}
+      ListHeaderComponent={TopoLista}
+    />
+  );
+}
+
+const estilos = StyleSheet.create({
+  titulo: {
+    fontSize: 20,
+    lineHeight: 32,
+    marginHorizontal: 16,
+    marginTop: 16,
+    fontWeight: 'bold',
+    color: '#464646',
+  },
+});
+
+export default Produtores;
